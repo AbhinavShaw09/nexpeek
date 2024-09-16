493c43c7d22521cd438340fd3e37120d2703b61d
abhinav09 <shawabhinav93@gmail.com>
2024-09-16 21:50:18 +0530

    🚀 added the utils folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import mockData from './utils/mockData.js'
 
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
-
-const mockData = [
-    { id: 1, companyName: "Amazon", ideas: 101 },
-    { id: 2, companyName: "Microsoft", ideas: 101 },
-    { id: 3, companyName: "CoinSwitch", ideas: 101 },
-    { id: 4, companyName: "Nirmata", ideas: 101 },
-    { id: 5, companyName: "Nvdia", ideas: 101 },
-]
 
 app.get('/home', (req, res) => {
     res.json(mockData);
---
+++ server/src/utils/mockData.js
@@ -0,0 +1,9 @@
+const mockData = [
+    { id: 1, companyName: "Amazon", ideas: 101 },
+    { id: 2, companyName: "Microsoft", ideas: 101 },
+    { id: 3, companyName: "CoinSwitch", ideas: 101 },
+    { id: 4, companyName: "Nirmata", ideas: 101 },
+    { id: 5, companyName: "Nvdia", ideas: 101 },
+]
+
+export default mockData;
